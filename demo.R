c5291a0f3765f310f48f1a493226086008a44e18
nd265 <35167740+nd265@users.noreply.github.com>
2021-11-18 12:10:53 -0800

    Added additional arg

--- demo.R
+++ demo.R
@@ -1,12 +1,13 @@
-# author: Tiffany Timbers
-# date: 2020-01-15
+# author: Navya Dahiya
+# date: 2021-11-18
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[--arg4=<arg4>]   Takes any value (this is an optional option)
 " -> doc
 
 library(docopt)
